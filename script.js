a33d342060f17cf808d9ad0a974cbc2fa80e2c32
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-04-12 20:48:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here
 
+let mode=document.querySelector("#swap");
+        let currentMode="Light";
+
+        mode.addEventListener("click",()=>{
+            if(currentMode==="Light"){
+                currentMode="Dark"
+                let body=document.querySelector("body");
+                body.style.backgroundColor="black";
+                body.style.color="white";
+				mode.style.color="white";
+				mode.style.backgroundColor="blue";
+                
+            }
+            else{
+                let body=document.querySelector("body");
+                body.style.backgroundColor="white";
+                body.style.color="black";
+                currentMode="Light"
+				mode.style.color="black";
+				mode.style.backgroundColor="silver";
+            }
+        })
